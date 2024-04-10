d8ea3dd95e01d5000d0b606b915d3053a5b55f3d
Adeshina Qudus <qudusa55@gmail.com>
2024-04-10 05:44:06 +0100

    refactor

--- src/searchpage/index.jsx
+++ src/searchpage/index.jsx
@@ -1,18 +1,12 @@
 import style from "./index.module.css"
 import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+// import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const SearchPage = ()=>{
     let url = "http://localhost:8080/api/v1/user/searchBook";
     const [title,setTitle] = useState("")
     const [book, addBook] = useState([])
-
-    // useEffect(() => {
-    //     const storedBook = JSON.parse(localStorage.getItem("book"));
-    //     if (storedBook) {
-    //         addBook(storedBook);
-    //     }
-    // }, []);
 
     const AddBook = (value)=>{
        const updated = [...book, {title: value.title, image : value.image}]
@@ -20,32 +14,23 @@ const SearchPage = ()=>{
         addBook(updated);
     }
 
-    const handleSubmit = async (e)=>{
-        let obj = {
-            "title": title,
-            "mail" : localStorage.getItem("mail")
-        }
-        const returnTitle = JSON.stringify(obj)
-        e.preventDefault();
-        await fetch(url,{
-            method :"POST",
-            headers:{
-                "Content-Type": "application/json",
-            },
-            body : returnTitle
-        }).then((response)=>{
-            return response.json();
-        }).then((response)=> {
-            console.log(response.data)
-            AddBook(response.data)
-        }).catch((error) => console.log(error))
-    }
+    const handleSubmit = async (values,{resetForm}) =>{
 
+        try {
+            const payload = {
+                mail: localStorage.getItem("mail"),
+                title: values.title
+            }
+            const response = await axios.post(url,payload)
+            AddBook(response.data)
+            console.log(response.data)
+            resetForm()
+        }catch (error){
+            console.log(error)
+        }
+    }
     return(
         <div className={style.mainCont}>
-            <div className={style.readingList}>
-                <h3>Reading List</h3>
-                <div>
                     {book.map(value => {
                         return(
                             <div>
@@ -54,10 +39,8 @@ const SearchPage = ()=>{
                             </div>
                         )
                     })}
-                </div>
-            </div>
             <div className={style.searchBar}>
-                <form onSubmit={(event)=> handleSubmit(event)}>
+                <form onSubmit={(event)=> handleSubmit}>
                     <input type={"text"}
                            key={"title"}
                            onChange={(e) => setTitle(e.target.value)}
